c0faa794799e4c11308eda01080aa975c83c2819
Rahul Kumar <irahulkv@gmail.com>
2022-06-07 08:50:13 +0530

    added array and objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let = logEntries = [];
 
 function getUserNumberInput() {
   return parseInt(userInput.value);
@@ -14,6 +15,14 @@ function add() {
   const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
+  const logEntry = {
+    opperation: "Add",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function subtract() {
